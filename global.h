b31527b50540c781fd6651c67305f132608ad3cc
LittleBird <ilms49898723@gmail.com>
2016-05-03 00:05:07 +0900

    initial

---
+++ global.h
@@ -0,0 +1,21 @@
+#ifndef NETWORK_PROGRAMMING_GLOBAL_H_
+#define NETWORK_PROGRAMMING_GLOBAL_H_
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <signal.h>
+#include <unistd.h>
+#include <cerrno>
+
+#endif /* NETWORK_PROGRAMMING_GLOBAL_H_ */
+
