0afa3ecf2b5e1f3d3e3716a7f301912b1b9db49f
LittleBird <ilms49898723@gmail.com>
2016-05-04 00:30:33 +0900

    add setSocketTomeout()

--- nputility.cpp
+++ nputility.cpp
@@ -1,11 +1,18 @@
 #include "nputility.h"
 
+void setSocketTimeout(const int& socketfd, const int& second, const int& millisecond) {
+    timeval tv;
+    tv.tv_sec = second;
+    tv.tv_usec = millisecond * 1000;
+    setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+}
+
 int stringHash(const char* src, size_t len) {
-    const int coef = 31;
+    int coef = 31;
     int ret = 0;
     for (size_t i = 0; i < len; ++i) {
         ret = ret * coef + src[i];
-        corf *= 31;
+        coef *= 31;
     }
     return ret;
 }
--- nputility.h
+++ nputility.h
@@ -1,6 +1,11 @@
 #ifndef NETWORK_PROGRAMMING_HOMEWORK_2_NPUTILITY_H
 #define NETWORK_PROGRAMMING_HOMEWORK_2_NPUTILITY_H
 
+#include <cstdlib>
+#include "global.h"
+
+void setSocketTimeout(const int& socketfd, const int& second, const int& millisecond);
+
 int stringHash(const char* src, size_t len);
 
 #endif // NETWORK_PROGRAMMING_HOMEWORK_2_NPUTILITY_H
