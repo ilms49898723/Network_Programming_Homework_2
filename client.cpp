12314fb56c80a4f61da8816eadadc446863eb536
LittleBird <ilms49898723@gmail.com>
2016-05-04 00:30:15 +0900

    initial functions

--- client.cpp
+++ client.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib>
 #include <cstring>
 #include "global.h"
+#include "nputility.h"
+
+constexpr int MAXN = 2048;
+
+void clientFunc(const int& fd, sockaddr_in serverAddr);
 
 int main(int argc, char const** argv) {
     // check argments
@@ -15,7 +20,7 @@ int main(int argc, char const** argv) {
         fprintf(stderr, "%s is not valid\n", argv[2]);
         exit(EXIT_FAILURE);
     }
-    // variables definition
+    // socket initialize
     int socketfd;
     sockaddr_in serverAddr;
     memset(&serverAddr, 0, sizeof(serverAddr));
@@ -23,5 +28,55 @@ int main(int argc, char const** argv) {
     serverAddr.sin_port = htons(port);
     inet_pton(AF_INET, argv[1], &serverAddr.sin_addr);
     socketfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // set socket timeout
+    setSocketTimeout(socketfd, 0, 200);
+    // run clientFunc
+    clientFunc(socketfd, serverAddr);
     return 0;
 }
+
+void clientFunc(const int& fd, sockaddr_in serverAddr) {
+    // fd_set initialize
+    fd_set fdset;
+    int maxfdp1 = fd + 1;
+    FD_ZERO(&fdset);
+    // server sockaddr*
+    sockaddr* serverAddrp = reinterpret_cast<sockaddr*>(&serverAddr);
+    // server socklen_t
+    socklen_t serverLen;
+    // loop to select
+    for ( ; ; ) {
+        // set socket fd
+        FD_SET(fd, &fdset);
+        FD_SET(fileno(stdin), &fdset);
+        int nready = select(maxfdp1, &fdset, NULL, NULL, NULL);
+        if (nready < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            else {
+                fprintf(stderr, "select: %s\n", strerror(errno));
+            }
+        }
+        if (FD_ISSET(fileno(stdin), &fdset)) {
+            // TODO: complete it
+            char buffer[MAXN];
+            serverLen = sizeof(serverAddr);
+            memset(buffer, 0, sizeof(buffer));
+            fgets(buffer, MAXN, stdin);
+            int m;
+            m = sendto(fd, buffer, 3, 0, serverAddrp, serverLen);
+            while ((m = recvfrom(fd, buffer, MAXN, 0, serverAddrp, &serverLen)) < 0) {
+                if (errno == EWOULDBLOCK) {
+                    fprintf(stderr, "socket timeout\n");
+                }
+                else {
+                    fprintf(stderr, "%s\n", strerror(errno));
+                }
+            }
+            printf("%s\n", buffer);
+            sendto(fd, buffer, strlen(buffer), 0, serverAddrp, serverLen);
+        }
+    }
+}
+
--- server.cpp
+++ server.cpp
@@ -2,6 +2,11 @@
 #include <cstdlib>
 #include <cstring>
 #include "global.h"
+#include "nputility.h"
+
+constexpr int MAXN = 2048;
+
+void serverFunc(const int& fd);
 
 int main(int argc, char const** argv) {
     // check arguments
@@ -9,16 +14,16 @@ int main(int argc, char const** argv) {
         fprintf(stderr, "usage %s <port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    // check port
+    // read port
     int port;
     if (sscanf(argv[1], "%d", &port) != 1) {
         fprintf(stderr, "port %s is not valid\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    // variables definition
-    int socketfd;
-    sockaddr_in serverAddr, clientAddr;
     // socket initialize
+    // socketfd, sockaddr_in
+    int socketfd;
+    sockaddr_in serverAddr;
     socketfd = socket(AF_INET, SOCK_DGRAM, 0);
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
@@ -26,6 +31,50 @@ int main(int argc, char const** argv) {
     serverAddr.sin_port = htons(port);
     // bind
     bind(socketfd, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr));
+    // set timeout
+    setSocketTimeout(socketfd, 0, 200);
+    // run server function
+    serverFunc(socketfd);
     return 0;
+}
+
+void serverFunc(const int& fd) {
+    // fd_set initialize
+    fd_set fdset;
+    int maxfdp1 = fd + 1;
+    FD_ZERO(&fdset);
+    // client sockaddr_in and its (sockaddr*)&sockaddr_in
+    sockaddr_in clientAddr;
+    sockaddr* clientAddrp = reinterpret_cast<sockaddr*>(&clientAddr);
+    // client socket_len
+    socklen_t clientLen;
+    // while loop to select
+    for ( ; ; ) {
+        // set socket fd
+        FD_SET(fd, &fdset);
+        printf("after select\n");
+        int nready = select(maxfdp1, &fdset, NULL, NULL, NULL);
+        if (nready < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            else {
+                fprintf(stderr, "select: %s\n", strerror(errno));
+            }
+        }
+        if (FD_ISSET(fd, &fdset)) {
+            // TODO: complete it
+            char buffer[MAXN];
+            clientLen = sizeof(clientAddr);
+            memset(buffer, 0, sizeof(buffer));
+            int n, m;
+            printf("wait recvfrom\n");
+            m = recvfrom(fd, buffer, MAXN, 0, clientAddrp, &clientLen);
+            strcpy(buffer, "WELCOME!!!");
+            printf("send %s to client\n", buffer);
+            n = sendto(fd, buffer, strlen(buffer), 0, clientAddrp, clientLen);
+            printf("send complete!\n");
+        }
+    }
 }
 
