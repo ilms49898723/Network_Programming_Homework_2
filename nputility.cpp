5c015e1b55f10ef79d2d11a3b40ed97ad99dafc7
LittleBird <ilms49898723@gmail.com>
2016-05-04 01:29:32 +0900

    add udp send, recv functions, may have problems

--- nputility.cpp
+++ nputility.cpp
@@ -1,10 +1,61 @@
 #include "nputility.h"
 
+void udpSendTo(const int& socketfd, const char* msg, const size_t n, sockaddr*& dstSocketp) {
+    socklen_t dstLen = sizeof(*dstSocketp);
+    char buffer[MAXN];
+    int byteSend, byteRecv;
+    if ((byteSend = sendto(socketfd, msg, n, 0, dstSocketp, dstLen)) < 0) {
+        fprintf(stderr, "udpSendTo: sendto: %s\n", strerror(errno));
+        return;
+    }
+    while (true) {
+        byteRecv = recvfrom(socketfd, buffer, MAXN, 0, dstSocketp, &dstLen);
+        if (byteRecv < 0) {
+            if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                if ((byteSend = sendto(socketfd, msg, n, 0, dstSocketp, dstLen)) < 0) {
+                    fprintf(stderr, "udpSendTo: sendto: %s\n", strerror(errno));
+                    return;
+                }
+            }
+            else {
+                fprintf(stderr, "udpSendTo: recvfrom: %s\n", strerror(errno));
+            }
+        }
+        else {
+            break;
+        }
+    }
+}
+
+void udpRecvFrom(const int& socketfd, char* dst, const size_t n, sockaddr*& srcSocketp) {
+    socklen_t srcLen = sizeof(*srcSocketp);
+    int byteSend, byteRead;
+    while ((byteRead = recvfrom(socketfd, dst, n, 0, srcSocketp, &srcLen)) < 0) {
+        if (errno == EWOULDBLOCK || errno == EAGAIN) {
+            continue;
+        }
+        else {
+            fprintf(stderr, "udpRecvFrom: recvfrom: %s\n", strerror(errno));
+        }
+    }
+    while ((byteSend = sendto(socketfd, dst, byteRead, 0, srcSocketp, srcLen)) < 0) {
+        fprintf(stderr, "udpRecvFrom: sendto: %s\n", strerror(errno));
+    }
+    dst[byteRead] = '\0';
+}
+
 void setSocketTimeout(const int& socketfd, const int& second, const int& millisecond) {
     timeval tv;
     tv.tv_sec = second;
     tv.tv_usec = millisecond * 1000;
     setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+}
+
+void trimNewLine(char* src) {
+    size_t len = strlen(src);
+    if (src[len - 1] == '\n') {
+        src[len - 1] = '\0';
+    }
 }
 
 int stringHash(const char* src, size_t len) {
--- nputility.h
+++ nputility.h
@@ -1,10 +1,19 @@
 #ifndef NETWORK_PROGRAMMING_HOMEWORK_2_NPUTILITY_H
 #define NETWORK_PROGRAMMING_HOMEWORK_2_NPUTILITY_H
 
+#include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <cerrno>
 #include "global.h"
 
+void udpSendTo(const int& socketfd, const char* msg, const size_t n, sockaddr*& dstSocketp);
+
+void udpRecvFrom(const int& socketfd, char* dst, const size_t n, sockaddr*& srcSocketp);
+
 void setSocketTimeout(const int& socketfd, const int& second, const int& millisecond);
+
+void trimNewLine(char* src);
 
 int stringHash(const char* src, size_t len);
 
