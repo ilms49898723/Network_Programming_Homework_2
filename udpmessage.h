8b80ec29c25c9240108890b9c236db0b0f86367f
LittleBird <ilms49898723@gmail.com>
2016-05-06 00:41:15 +0900

    add some constant string

--- udpmessage.h
+++ udpmessage.h
@@ -3,16 +3,20 @@
 
 #include <string>
 
-const std::string msgOptWELCOME = "[L]Login  [R]Register\n";
+const std::string msgOptWELCOME = "[L]Login  [R]Register  [Q]Quit\n";
 const std::string msgOptARTICLE = "[R]Response  [L]Like  [R]Return";
-const std::string msgOptMAIN = std::string("[SP]Show Profile  [SA]Show Article  [A]Add Article\n") +
-                               std::string("[E]Enter Article [C]Chat [S]Search [L]Logout\n");
+const std::string msgOptMAIN = std::string("[SP]Show Profile [SE]Set Profile\n") +
+                               std::string("[SA]Show Article [A]Add Article [E]Enter Article\n") +
+                               std::string("[C]Chat [S]Search [L]Logout\n");
 
 const std::string msgNEWCONNECTION = "NEWCONNECTION";
 const std::string msgREGISTER = "REGISTER";
 const std::string msgLOGIN = "LOGIN";
 const std::string msgLOGOUT = "LOGOUT";
 const std::string msgSHOWPROFILE = "SHOWPROFILE";
+const std::string msgSETPROFILE = "SETPROFILE";
+const std::string msgSHOWARTICLE = "SHOWARTICLE";
+const std::string msgADDARTICLE = "ADDARTICLE";
 
 #endif // NETWORK_PROGRAMMING_HOMEWORK_2_UDPMESSAGE_H_
 
