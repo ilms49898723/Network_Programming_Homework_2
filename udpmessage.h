891e176eb388be6c2cae4e9ea611f12ba2519e90
LittleBird <ilms49898723@gmail.com>
2016-05-06 15:00:07 +0900

    add message definitions

--- udpmessage.h
+++ udpmessage.h
@@ -7,12 +7,14 @@ const std::string msgOptWELCOME = "[L]Login  [R]Register  [Q]Quit\n";
 const std::string msgOptARTICLE = "[R]Response  [L]Like  [R]Return";
 const std::string msgOptMAIN = std::string("[SP]Show Profile [SE]Set Profile\n") +
                                std::string("[SA]Show Article [A]Add Article [E]Enter Article\n") +
-                               std::string("[C]Chat [S]Search [L]Logout\n");
+                               std::string("[C]Chat [S]Search User\n") +
+                               std::string("[L]Logout [DA]Delete Account\n");
 
 const std::string msgNEWCONNECTION = "NEWCONNECTION";
 const std::string msgREGISTER = "REGISTER";
 const std::string msgLOGIN = "LOGIN";
 const std::string msgLOGOUT = "LOGOUT";
+const std::string msgDELETEACCOUNT = "DELETEACCOUNT";
 const std::string msgSHOWPROFILE = "SHOWPROFILE";
 const std::string msgSETPROFILE = "SETPROFILE";
 const std::string msgADDARTICLE = "ADDARTICLE";
